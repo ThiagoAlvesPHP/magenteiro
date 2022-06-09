38721f1c24124a6eb9455eaeb86904ce7d4aa3e9
ThiagoAlvesPHP <thicoalves@hotmail.com>
2022-06-09 11:04:12 -0300

    incluindo lib e usando na pratica

--- app/code/JsDeveloper/Js/view/frontend/web/js/register.js
+++ app/code/JsDeveloper/Js/view/frontend/web/js/register.js
@@ -1,6 +1,11 @@
 require(
-    ['jquery'],
+    [
+        'jquery',
+        'JsDeveloper_Js/js/lib/jquery.mask'
+    ],
     function($) {
         console.log('TEXT/X-MAGENTO-INIT');
+
+        // $('#email_address').mask('000.000.000-00');
     }
 )
