cc94a70ac6528cdd6eb211a4d4788728cf65eb0f
ThiagoAlvesPHP <thicoalves@hotmail.com>
2022-06-09 10:41:54 -0300

    inserindo js via xml

--- app/etc/config.php
+++ app/etc/config.php
@@ -255,6 +255,7 @@ return [
         'Dotdigitalgroup_Email' => 1,
         'Dotdigitalgroup_Chat' => 1,
         'ForMage_Theme' => 1,
+        'JsDeveloper_Js' => 1,
         'Klarna_Core' => 1,
         'Klarna_Ordermanagement' => 1,
         'Klarna_Kp' => 1,
