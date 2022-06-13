ce058e901df9025417b70ef69ab411fd78c2c216
ThiagoAlvesPHP <thicoalves@hotmail.com>
2022-06-13 15:37:57 -0300

    criando rota nova

---
+++ app/code/Magenteiro/HoraCerta/Controller/Index/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Magenteiro\HoraCerta\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class Index implements HttpGetActionInterface
+{
+    public function execute()
+    {
+        die('Funcionou');
+    }
+}
--- app/code/Magenteiro/HoraCerta/registration.php
+++ app/code/Magenteiro/HoraCerta/registration.php
@@ -6,4 +6,4 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magenteiro_CustomerNormalizer', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magenteiro_HoraCerta', __DIR__);
