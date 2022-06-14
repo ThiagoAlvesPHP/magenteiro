e6268070f044e560a3e417b19dcb1850beca4b8d
ThiagoAlvesPHP <thicoalves@hotmail.com>
2022-06-14 10:19:31 -0300

    router exibindo data e hora

--- app/code/Magenteiro/HoraCerta/Controller/Index/Index.php
+++ app/code/Magenteiro/HoraCerta/Controller/Index/Index.php
@@ -3,11 +3,22 @@
 namespace Magenteiro\HoraCerta\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Stdlib\DateTime\Timezone;
 
 class Index implements HttpGetActionInterface
 {
+    private $timezone;
+
+    public function  __construct(Timezone $timezone)
+    {
+        $this->timezone = $timezone;
+    }
+
     public function execute()
     {
-        die('Funcionou');
+        // echo $this->timezone->getConfigTimezone();
+
+        echo $this->timezone->convertConfigTimeToUtc($this->timezone->date());
+        exit;
     }
 }
