fb2b428a0184588bdac2cc242807aebb32ccbc35
ThiagoAlvesPHP <thicoalves@hotmail.com>
2022-06-14 15:20:46 -0300

    padrão novo usa implements

--- app/code/Magenteiro/HoraCerta/Controller/Index/Index.php
+++ app/code/Magenteiro/HoraCerta/Controller/Index/Index.php
@@ -3,22 +3,25 @@
 namespace Magenteiro\HoraCerta\Controller\Index;
 
 use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\Stdlib\DateTime\Timezone;
 
 class Index implements HttpGetActionInterface
 {
     private $timezone;
+    private $resultFactory;
 
-    public function  __construct(Timezone $timezone)
+    public function  __construct(Timezone $timezone, ResultFactory $resultFactory)
     {
         $this->timezone = $timezone;
+        $this->resultFactory = $resultFactory;
     }
 
     public function execute()
     {
-        // echo $this->timezone->getConfigTimezone();
-
-        echo $this->timezone->convertConfigTimeToUtc($this->timezone->date());
-        exit;
+        $conteudo = $this->timezone->convertConfigTimeToUtc($this->timezone->date());
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW);
+        $result->setContents($conteudo);
+        return $result;
     }
 }
