6fcf1a602c34e7eed8169c08d6a52108f83b53f4
ThiagoAlvesPHP <thicoalves@hotmail.com>
2022-06-20 09:25:30 -0300

    criaçãod e blockx criação de botão de compartilhar no facebook

---
+++ app/code/Magenteiro/Sharer/Block/Sharer.php
@@ -0,0 +1,21 @@
+<?php
+namespace Magenteiro\Sharer\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Store\Model\StoreManagerInterface;
+
+class Sharer extends Template
+{
+    private $storeManager;
+
+    public function __construct(Template\Context $context, array $data = [], StoreManagerInterface $storeManager)
+    {
+        parent::__construct($context, $data);
+        $this->storeManager = $storeManager;
+    }
+
+    public function getCurrentUrl()
+    {
+        return urlencode($this->storeManager->getStore()->getCurrentUrl());
+    }
+}
--- app/code/Magenteiro/Sharer/registration.php
+++ app/code/Magenteiro/Sharer/registration.php
@@ -6,4 +6,4 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magenteiro_Comments', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magenteiro_Sharer', __DIR__);
